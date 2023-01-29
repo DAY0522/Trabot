c8d60e35b639998bafb39750857efd1607e8702d
cutiepazzipozzi <sk000801@naver.com>
2023-01-29 18:25:59 +0900

    feat: 여행지 세부정보 페이지 추가
    /detail?contentId={아이디}
    아이디 부분에 api 요청에 필요한 아이디를 입력하면 detail 페이지에서 해당 정보를 띄워줌

--- demo/application.py
+++ demo/application.py
@@ -6,13 +6,14 @@
 import sys
 sys.path.append('C:/Users/ekdud/Documents/Trabot/trabot')
 
-from flask import render_template
+from flask import render_template, request, jsonify
 
 from kochat.app import KochatApi
 from kochat.data import Dataset
 from kochat.loss import CRFLoss, CosFace, CenterLoss, COCOLoss, CrossEntropyLoss
 from kochat.model import intent, embed, entity
 from kochat.proc import DistanceClassifier, GensimEmbedder, EntityRecognizer, SoftmaxClassifier
+import requests
 
 from scenario import dust, weather, travel, restaurant
 # 에러 나면 이걸로 실행해보세요!
@@ -41,11 +42,27 @@ kochat = KochatApi(
     ]
 )
 
-
 @kochat.app.route('/')
 def index():
     return render_template("index2.html")
 
+@kochat.app.route('/detail')
+def detail():
+    return render_template("detail.html")
+
+@kochat.app.route('/get/detail', methods=['POST'])
+def getDetail():
+    contentId = request.get_json()["contentId"]
+    response = requests.get(f'https://apis.data.go.kr/B551011/KorService/detailCommon?serviceKey=7ut0kiJb%2FugaTORVPbk2lljMu0y9IY4HoAzWysfXZIKqVl%2FDJ7zsr6Ca3b7nwotssH2lFdHHms7yUOl2RTCgcA%3D%3D&MobileOS=ETC&MobileApp=AppTest&_type=json&contentId={contentId}&defaultYN=Y&firstImageYN=Y&areacodeYN=Y&catcodeYN=Y&addrinfoYN=Y&mapinfoYN=Y&overviewYN=Y',verify=False)
+    jsonData = response.json()['response']['body']['items']['item'][0]
+
+    return jsonify({
+        'title': jsonData['title'],
+        'addr1': jsonData['addr1'],
+        'tel': jsonData['tel'],
+        'overview': jsonData['overview'],
+        'img': jsonData['firstimage']
+    })
 
 if __name__ == '__main__':
     kochat.app.template_folder = kochat.root_dir + 'templates'
